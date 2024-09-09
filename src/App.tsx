d0a2ea62570da448254c8868911d12b9f797d8cf
Nathan Bierema <nbierema@gmail.com>
2024-09-08 23:20:59 -0400

    Add simple table

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Chip from '@mui/material/Chip';
+import CssBaseline from '@mui/material/CssBaseline';
+import Paper from '@mui/material/Paper';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
-function App() {
-  const [count, setCount] = useState(0)
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
+export default function BasicTable() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>Chief Surgery</TableCell>
+              <TableCell>Chief Trauma</TableCell>
+              <TableCell>PGY-2 Trauma</TableCell>
+              <TableCell>PGY-1 POG</TableCell>
+              <TableCell>PGY-1 CTV</TableCell>
+              <TableCell>PGY-1 Trauma</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>July 1</TableCell>
+              <TableCell>
+                <Chip label="Alice" />
+              </TableCell>
+              <TableCell>
+                <Chip label="Bob" />
+              </TableCell>
+              <TableCell>
+                <Chip label="Charlie" />
+              </TableCell>
+              <TableCell>
+                <Chip label="Dede" />
+              </TableCell>
+              <TableCell>
+                <Chip label="Earn" />
+              </TableCell>
+              <TableCell>
+                <Chip label="Frank" />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </ThemeProvider>
+  );
 }
-
-export default App
